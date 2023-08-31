6af4e86213b75308ef9992ce3bb5de15d4e9f104
thanhnv <thanhnv@rabiloo.com>
2023-08-31 16:37:28 +0700

    create project

---
+++ resources/js/Pages/User/Auth/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <Head title="Đăng nhập"/>
+    <div class="h-screen flex justify-center items-center">
+        <div class="m-auto bg-[red] form-login">
+            
+        </div>
+    </div>
+</template>
+<script>
+    import { Head, Link, useForm } from '@inertiajs/vue3';
+
+    export default {
+        components: {
+            Head,
+            Link
+        },
+        async created() {
+        },
+        data: function () {
+            return {
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
